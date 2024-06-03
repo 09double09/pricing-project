53fa29c885edc64bc36862b7ff119a4ae0636be4
jerry <nm40209@gmail.com>
2024-06-04 00:00:05 +0800

    要被當了

--- src/App.jsx
+++ src/App.jsx
@@ -11,49 +11,52 @@ const App = () => {
   let [designPrice, setDesignPrice] = useState([]);
   let [itemName, setItemName] = useState();
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <DesignList
-          itemName={itemName}
-          setItemName={setItemName}
-          designPrice={designPrice}
-          setDesignPrice={setDesignPrice}
-          item={item}
-          setItem={setItem}
-          designList={designList}
-          setDesignList={setDesignList}
-          itemPrice={itemPrice}
-          setItemPrice={itemPrice}
-        ></DesignList>
-      ),
-    },
-    {
-      path: "/itemSearch",
-      element: (
-        <ItemSearch
-          item={item}
-          setItem={setItem}
-          itemPrice={itemPrice}
-          setItemPrice={setItemPrice}
-        ></ItemSearch>
-      ),
-    },
-    {
-      path: "/designInformation",
-      element: (
-        <DesignInformation
-          itemName={itemName}
-          setItemName={setItemName}
-          item={item}
-          setItem={setItem}
-          designPrice={designPrice}
-          setDesignPrice={setDesignPrice}
-        ></DesignInformation>
-      ),
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: (
+          <DesignList
+            itemName={itemName}
+            setItemName={setItemName}
+            designPrice={designPrice}
+            setDesignPrice={setDesignPrice}
+            item={item}
+            setItem={setItem}
+            designList={designList}
+            setDesignList={setDesignList}
+            itemPrice={itemPrice}
+            setItemPrice={itemPrice}
+          ></DesignList>
+        ),
+      },
+      {
+        path: "/itemSearch",
+        element: (
+          <ItemSearch
+            item={item}
+            setItem={setItem}
+            itemPrice={itemPrice}
+            setItemPrice={setItemPrice}
+          ></ItemSearch>
+        ),
+      },
+      {
+        path: "/designInformation",
+        element: (
+          <DesignInformation
+            itemName={itemName}
+            setItemName={setItemName}
+            item={item}
+            setItem={setItem}
+            designPrice={designPrice}
+            setDesignPrice={setDesignPrice}
+          ></DesignInformation>
+        ),
+      },
+    ],
+    { basename: process.env.PUBLIC_URL }
+  );
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
